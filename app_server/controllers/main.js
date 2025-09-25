49572c2e24dbc9c30af632bdbb84e7b669a266f1
Gray Lott <gray.lott@hexhiker.com>
2025-09-25 18:46:10 -0400

    Completed Module 3

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,12 +1,21 @@
+var fs = require('fs');
+var roomsData = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+var mealsData = JSON.parse(fs.readFileSync('./data/meals.json', 'utf8'));
+
 const index = (req, res) => {
   res.render('index', { title: 'Express' });
 };
 
-const travlr = (req, res) => {
-  res.render('travlr', { title: 'Travlr' });
+const rooms = (req, res) => {
+  res.render('rooms', { title: 'Rooms', rooms: roomsData });
+};
+
+const meals = (req, res) => {
+  res.render('meals', { title: 'Meals', meals: mealsData });
 };
 
 module.exports = {
   index,
-  travlr
+  rooms,
+  meals
 };
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,10 +5,11 @@ var ctrlMain = require('../controllers/main');
 /* GET home page. */
 router.get('/', ctrlMain.index);
 
-/* GET travlr page. */
-router.get('/travlr', ctrlMain.travlr);
 
-/* GET travel page. */
-router.get('/travel', ctrlMain.travlr);
+/* GET rooms page. */
+router.get('/rooms', ctrlMain.rooms);
+
+/* GET meals page. */
+router.get('/meals', ctrlMain.meals);
 
 module.exports = router;
