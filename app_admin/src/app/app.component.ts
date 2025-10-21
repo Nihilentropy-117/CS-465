c973463c1aa107cc94220adf785f70d2b811cda3
Gray Lott <gray.lott@hexhiker.com>
2025-10-21 19:39:09 -0400

    complete

--- app_admin/src/app/app.component.ts
+++ app_admin/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TripListingComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'travlr-admin';
+  title = 'Travlr Getaways Admin!';
 }
